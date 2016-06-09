fd09f71ddad4723a92a5b145a160a1439a091499
firegrind <firegrind@gmail.com>
2016-06-09 21:15:28 +0200

    Improved feedback

--- prepare/setup.sh
+++ prepare/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-echo Set up target machine
+echo = Set up target machine
 echo
-echo First, ensure that the required applications to create state management are available
-
+echo == First, ensure that the required applications to create state management are available
 echo
 
 
@@ -14,14 +13,14 @@ PUPPET=$( which puppet )
 
 
 if [ "${GIT}" = "" ] ; then
-  echo Installing git
+  echo = Installing git
   sudo apt-get install -y git
 else
-  echo Found ${GIT}
+  echo = Found ${GIT}
 fi
 
 if [ "${PUPPET}" = "" ] ; then
-  echo installing puppet-common from apt.puppetlabs.com as the version in the ubuntu repository does not provide the module subcommand
+  echo = Installing puppet-common from apt.puppetlabs.com as the version in the ubuntu repository does not provide the module subcommand
   DEB=puppetlabs-release-precise.deb
   SOURCE=http://apt.puppetlabs.com/${DEB}
   DESTINATION=/tmp/${DEB}
@@ -30,12 +29,12 @@ if [ "${PUPPET}" = "" ] ; then
   sudo apt-get update
   sudo apt-get install puppet-common
 else
-  echo Found ${PUPPET}, checking for a version with the module subcommand
+  echo = Found ${PUPPET}, checking for a version with the module subcommand
   puppet --version
 fi
 
 
-echo Installing PuppetLabs Apache module
+echo = Installing PuppetLabs Apache module
 
 sudo puppet module install puppetlabs-apache
 
@@ -43,21 +42,19 @@ sudo puppet module install puppetlabs-apache
 REPOSITORY="https://github.com/infinity-technical/platform-engineering.git"
 
 
-echo Cloning state management system
-
-echo Clearing any existing files
+echo = Cloning state management system
 
 if [ -d platform-engineering ] ; then
-  echo Removing platform-engineering/
+  echo == Removing legacy platform-engineering/
   rm -rf platform-engineering
 fi
 
 
-echo Downloading repository
-
+echo == Downloading repository
 echo
 
 git clone ${REPOSITORY}
 
+echo = State management
 
 sudo puppet apply --verbose --detailed-exitcodes --noop platform-engineering/puppet/manifests/main.pp
