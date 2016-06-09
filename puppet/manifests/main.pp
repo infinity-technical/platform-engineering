2196e71607b28df976b5906507033e195f7a97aa
firegrind <firegrind@gmail.com>
2016-06-09 20:23:27 +0200

    start simple ...

--- puppet/manifests/main.pp
+++ puppet/manifests/main.pp
@@ -6,16 +6,4 @@
 
 # https://github.com/puppetlabs/puppetlabs-apache
 
-class { 'apache' :
-
-    # this class models the installation and configuration of the apache web server package
-    # the version parameter defaults to latest
-
-    apache::vhost { 'localhost' :
-        port => 80,
-        docroot => '/opt/web/www',
-        docroot_owner => 'www-data',
-        docroot_group => 'www-data',
-    }
-
-}
+class { 'apache' : }
